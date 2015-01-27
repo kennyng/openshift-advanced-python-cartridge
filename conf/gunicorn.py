e33669297a0d1e19f01be9b3cbf40e256e656558
Kenny Ng <kennyng.zq@gmail.com>
2015-01-27 17:59:08 -0600

    Removed gevent when using Python 3.

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -6,7 +6,7 @@ tmp_dir = os.environ["OPENSHIFT_TMP_DIR"]
 
 workers = os.environ.get("OPENSHIFT_PYTHON_WORKERS",
                          multiprocessing.cpu_count() * 2 + 1)
-worker_class = os.environ.get("OPENSHIFT_PYTHON_WORKER_CLASS", "gevent")
+worker_class = os.environ.get("OPENSHIFT_PYTHON_WORKER_CLASS", "sync")
 daemon = True
 bind = "unix:{0}run/appserver.sock".format(cart_dir)
 pidfile = "{0}run/appserver.pid".format(cart_dir)
